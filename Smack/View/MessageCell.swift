9b3bf57ab2863e93c4954cff65070884fba5ede8
Wirunpong Jaingamlertwong <wirunpong.j@outlook.com>
2018-01-18 16:46:31 +0700

    convert datetime of chat messages

--- Smack/View/MessageCell.swift
+++ Smack/View/MessageCell.swift
@@ -23,8 +23,23 @@ class MessageCell: UITableViewCell {
         userImage.image = UIImage(named: message.userAvatar)
         userImage.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
         userNameLabel.text = message.userName
-//        timeStampLabel.text = message.timeStamp
         messageLabel.text = message.message
+        
+        // convert datetime 2017-07-13T21:49:25Z
+        guard var isoDate = message.timeStamp else { return }
+        let end = isoDate.index(isoDate.endIndex, offsetBy: -5)
+        isoDate = isoDate.substring(to: end)
+        
+        let isoFormatter = ISO8601DateFormatter()
+        let chatDate = isoFormatter.date(from: isoDate.appending("Z"))
+        
+        let newFormatter = DateFormatter()
+        newFormatter.dateFormat = "MMM d, h:mm a"
+        
+        if let finalDate = chatDate {
+            let finalDate = newFormatter.string(from: finalDate)
+            timeStampLabel.text = finalDate
+        }
     }
 
 }
